6676d1b02fb562f64af9694762c6b59ceb707001
Steve Reinke <reinke@webit.de>
2016-09-09 11:34:17 +0200

    add test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,31 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_group 'Decorators', 'app/decorators'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/rails'
+require 'mocha/mini_test'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+# To add Capybara feature tests add `gem "minitest-rails-capybara"`
+# to the test group in the Gemfile and uncomment the following:
+require 'minitest/rails/capybara'
+
+# Uncomment for awesome colorful output
+# require "minitest/pride"
+
+require 'pp'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # TODO: uncomment this if you want to use factory girl
+  # include FactoryGirl::Syntax::Methods
 end
