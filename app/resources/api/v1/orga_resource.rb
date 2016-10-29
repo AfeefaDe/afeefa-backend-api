41857da456e0d7bee72901a654e9465c0ae6c6ee
foobar0112 <foobar2@posteo.de>
2016-10-30 00:46:18 +0200

    [FIX] orga creation

--- app/resources/api/v1/orga_resource.rb
+++ app/resources/api/v1/orga_resource.rb
@@ -4,9 +4,9 @@ class Api::V1::OrgaResource < Api::V1::BaseResource
   has_one :parent_orga, class_name: 'Orga', foreign_key: 'parent_id'
   has_many :sub_orgas, class_name: 'Orga', foreign_key: 'children_ids'
 
-  has_one :annotation, class_name: 'Annotation', foreign_key: 'annotateable_id'
-  has_one :location, class_name: 'Location', foreign_key: 'locateable_id'
-  has_one :contact_info, class_name: 'ContactInfo', foreign_key: 'contactable_id'
+  has_many :annotations, class_name: 'Annotation'
+  has_many :locations, class_name: 'Location'
+  has_many :contact_infos, class_name: 'ContactInfo'
 
   filter :title, apply: ->(records, value, _options) {
     records.where('title like ?', value)
