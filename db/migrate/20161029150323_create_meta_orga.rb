924542e6c3e6dc64bc94b63b2eb98adf3720436c
foobar0112 <foobar2@posteo.de>
2016-10-30 13:18:35 +0100

    [FIX] fixed state_changed_at timestamp

--- db/migrate/20161029150323_create_meta_orga.rb
+++
@@ -1,7 +0,0 @@
-require File.join(__dir__, '..', 'seeds')
-
-class CreateMetaOrga < ActiveRecord::Migration[5.0]
-  def change
-    Seeds.recreate_all
-  end
-end
--- db/migrate/20161029232522_add_state_changed_at_to_things_and_orga.rb
+++ db/migrate/20161029232522_add_state_changed_at_to_things_and_orga.rb
@@ -1,7 +1,7 @@
 class AddStateChangedAtToThingsAndOrga < ActiveRecord::Migration[5.0]
   def change
-    add_column :events, :state_changed_at, :string
-    add_column :orgas, :state_changed_at, :string
+    add_column :events, :state_changed_at, :datetime
+    add_column :orgas, :state_changed_at, :datetime
     # todo: add category columns for other things
   end
 end
