36335644e3b1bc962590a54e54a3b339a3335fd1
Steve Reinke <reinke@webit.de>
2016-12-14 12:57:22 +0100

    [FEATURE] category and sub category as relation in orga resource, refs #61, #62

--- app/resources/api/v1/orga_resource.rb
+++ app/resources/api/v1/orga_resource.rb
@@ -1,8 +1,7 @@
 class Api::V1::OrgaResource < Api::V1::BaseResource
 
   attributes :title, :description, :created_at, :updated_at,
-             :state_changed_at, :state, :state_transition,
-             :category
+             :state_changed_at, :state, :state_transition
 
   has_one :parent_orga, class_name: 'Orga', foreign_key: 'parent_id'
   has_many :sub_orgas, class_name: 'Orga', foreign_key: 'children_ids'
@@ -11,6 +10,9 @@ class Api::V1::OrgaResource < Api::V1::BaseResource
   has_many :locations, class_name: 'Location'
   has_many :contact_infos, class_name: 'ContactInfo'
   # has_many :events, class_name: 'Event'
+
+  belongs_to :category
+  belongs_to :sub_category, class_name: 'Category'
 
   # paginator :offset
 
