ff72de4d62c02d7669f1acf0d7e23e9a6f645337
Steve Reinke <reinke@webit.de>
2017-02-01 19:34:24 +0100

    test for reproducing create and try to update error

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,24 @@ class Api::V1::EventsController < Api::V1::BaseController
     super
   end
 
+  # def update
+  #   event = Event.undeleted.find(params[:id])
+  #   if event.update(map_params(resource_params))
+  #     jsonapi_render json: event
+  #   else
+  #     jsonapi_render_errors json: event, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # protected
+  #
+  # def map_params(params)
+  #   params[:time_start] = params.delete(:has_time_start)
+  #   params[:time_end] = params.delete(:has_time_end)
+  #   params.delete(:created_at)
+  #   params.delete(:updated_at)
+  #   pp params
+  #   params
+  # end
+
 end
