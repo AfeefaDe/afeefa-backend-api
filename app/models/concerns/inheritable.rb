6e920639137fd499b6091a0a22a7654da899c4a1
Steve Reinke <reinke@webit.de>
2017-05-31 10:01:27 +0200

    [FEATURE] no validations on deactivate, refs #204

--- app/models/concerns/inheritable.rb
+++ app/models/concerns/inheritable.rb
@@ -6,7 +6,8 @@ module Inheritable
     /\A(short_description|contact_infos|locations)(\|(short_description|contact_infos|locations))*\z/
 
   included do
-    validates_format_of :inheritance, with: INHERITABLE_ATTRIBUTES_REGEX, allow_blank: true
+    validates_format_of :inheritance, with: INHERITABLE_ATTRIBUTES_REGEX, allow_blank: true,
+      unless: :skip_all_validations?
   end
 
 end
