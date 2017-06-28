1ff8a0ed8ded65af43b9a6e34290bcc57062532a
foobar0112 <foobar2@posteo.de>
2017-06-28 15:52:26 +0200

    added related model name

--- app/resources/api/v1/annotation_resource.rb
+++ app/resources/api/v1/annotation_resource.rb
@@ -1,3 +1,5 @@
 class Api::V1::AnnotationResource < Api::V1::BaseResource
 
+  model_name 'Annotation'
+
 end
--- app/resources/api/v1/location_resource.rb
+++ app/resources/api/v1/location_resource.rb
@@ -1,3 +1,5 @@
 class Api::V1::LocationResource < Api::V1::BaseResource
 
+  model_name 'Location'
+
 end
