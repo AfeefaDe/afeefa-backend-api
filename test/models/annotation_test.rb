8388527f6852603312807a77e8e53fca0bc341e8
Steve Reinke <reinke@webit.de>
2017-07-05 12:18:43 +0200

    [BUGFIX] fix annotation tests

--- test/models/annotation_test.rb
+++ test/models/annotation_test.rb
@@ -3,14 +3,24 @@ require 'test_helper'
 class AnnotationTest < ActiveSupport::TestCase
 
   should 'render json' do
-    object_keys = [:id, :type, :attributes, :relationships]
-    attribute_keys = [:detail]
-    relationships = [:annotation_category, :entry]
-    todo = Annotation.new(entry: create(:orga), annotation_category: AnnotationCategory.first, detail: 'FooBar')
-    assert todo.save, todo.errors.messages
-    json = JSON.parse(todo.to_json).deep_symbolize_keys
+    object_keys = [:id, :type, :attributes]
+    attribute_keys = [:annotation_category_id, :detail]
+    annotation = Annotation.new(entry: create(:orga), annotation_category: AnnotationCategory.first, detail: 'FooBar')
+    assert annotation.save, annotation.errors.messages
+    json = JSON.parse(annotation.to_json).deep_symbolize_keys
     assert_equal(object_keys.sort, json.keys.sort)
     assert_equal(attribute_keys.sort, json[:attributes].keys.sort)
+    assert_not json.key?(:relationships)
+  end
+
+  should 'render todos json' do
+    object_keys = [:id, :type, :relationships]
+    relationships = [:annotation, :annotation_category, :entry]
+    todo = Annotation.new(entry: create(:orga), annotation_category: AnnotationCategory.first, detail: 'FooBar')
+    assert todo.save, todo.errors.messages
+    json = JSON.parse(todo.to_todos_hash.to_json).deep_symbolize_keys
+    assert_equal(object_keys.sort, json.keys.sort)
+    assert_not json.key?(:attributes)
     assert_equal(relationships.sort, json[:relationships].keys.sort)
     relationships.each do |relation|
       assert_equal [:data], json[:relationships][relation].keys
@@ -19,7 +29,14 @@ class AnnotationTest < ActiveSupport::TestCase
           assert_equal todo.send(relation)[index].to_hash, element
         end
       else
-        assert_equal todo.send(relation).to_hash(attributes: nil, relationships: nil), data
+        expected =
+          if todo.respond_to?("#{relation}_to_hash")
+            todo.send("#{relation}_to_hash")
+          else
+            todo.send(relation).to_hash(attributes: nil, relationships: nil)
+          end
+        # only care for type and id
+        assert_equal expected.slice(:type, :id), data.slice(:type, :id)
       end
     end
   end
