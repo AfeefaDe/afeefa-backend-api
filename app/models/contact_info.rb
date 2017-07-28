261d7bd290a2170752ed10b11108d5cf570691ed
hagenbeelitz <you@example.com>
2017-07-28 16:09:05 +0200

    #308 add fax as attribute

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -4,19 +4,20 @@ class ContactInfo < ApplicationRecord
 
   # CONSTANTS
   # PHONE = 'phone'
+  # FAX = 'fax'
   # MAIL = 'mail'
   # WEB = 'web'
   # FACEBOOK = 'facebook'
   # TWITTER = 'twitter'
   # FREE_INFO ='free_info'
-  # TYPES = [PHONE, MAIL, WEB, FACEBOOK, TWITTER, FREE_INFO]
+  # TYPES = [PHONE, FAX, MAIL, WEB,< FACEBOOK, TWITTER, FREE_INFO]
 
   # ATTRIBUTES AND ASSOCIATIONS
   belongs_to :contactable, polymorphic: true
 
   # VALIDATIONS
   # validates_presence_of :contactable
-  # validate :ensure_mail_or_phone
+  # validate :ensure_mail_or_phone_or_fax
 
   # CLASS METHODS
   class << self
@@ -25,7 +26,7 @@ class ContactInfo < ApplicationRecord
     end
 
     def default_attributes_for_json
-      %i(contact_person mail phone web social_media opening_hours spoken_languages).freeze
+      %i(contact_person mail phone fax web social_media opening_hours spoken_languages).freeze
     end
 
     def relation_whitelist_for_json
@@ -40,9 +41,10 @@ class ContactInfo < ApplicationRecord
   private
 
   def ensure_mail_or_phone
-    if mail.blank? && phone.blank?
-      errors.add(:mail, 'Mail-Adresse oder Telefonnummer muss angegeben werden.')
-      errors.add(:phone, 'Mail-Adresse oder Telefonnummer muss angegeben werden.')
+    if mail.blank? && phone.blank? && fax.blank?
+      errors.add(:mail, 'Mail-Adresse, Telefonnummer oder Fax muss angegeben werden.')
+      errors.add(:phone, 'Mail-Adresse, Telefonnummer oder Fax muss angegeben werden.')
+      errors.add(:fax, 'Mail-Adresse, Telefonnummer oder Fax muss angegeben werden.')
     end
   end
 end
