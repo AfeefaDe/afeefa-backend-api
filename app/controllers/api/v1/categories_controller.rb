4babd319a32199437541bc198d6c247f22a42733
Steve Reinke <steve_is@gmx.de>
2017-09-13 16:00:10 +0200

    [FEATURE] area filter as default for categories, refs #336

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,6 +2,25 @@ class Api::V1::CategoriesController < Api::V1::BaseController
 
   private
 
+  def apply_custom_filter!(filter, filter_criterion, objects)
+    objects =
+      case filter.to_sym
+      when :area
+        objects.by_area(filter_criterion)
+      else
+        objects
+      end
+    objects
+  end
+
+  def custom_filter_whitelist
+    %w(area).freeze
+  end
+
+  def default_filter
+    { area: current_api_v1_user.area }
+  end
+
   def do_includes!(objects)
     objects =
       objects.includes(:parent)
--- test/controllers/api/v1/categories_controller_test.rb
+++ test/controllers/api/v1/categories_controller_test.rb
@@ -8,11 +8,21 @@ class Api::V1::CategoriesControllerTest < ActionController::TestCase
     end
 
     should 'I want to get all categories' do
+      area = @controller.current_api_v1_user.area
+
+      other_category = Category.last
+      other_category.area = area + '_other'
+      assert other_category.save
+
+      categories = Category.by_area(area)
+      assert categories.count < Category.count
+      assert_not_includes categories, other_category
+
       get :index
       assert_response :ok, response.body
       json = JSON.parse(response.body)
       assert_kind_of Array, json['data']
-      assert_equal Category.count, json['data'].size
+      assert_equal categories.count, json['data'].size
       json['data'].each do |category_json|
         assert id = category_json['id']
         assert category = Category.find(id)
