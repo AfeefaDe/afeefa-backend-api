89d7e5cc93b967a5003c60d74bfbd4537ef8c5c5
Steve Reinke <steve_is@gmx.de>
2017-10-02 15:19:14 +0200

    [FEATURE] translation sync job for maintenance, refs #322

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,9 @@
 set :chronic_options, hours24: true
 
-every 1.day, at: '00:01' do
-  runner 'TranslationSyncJob.perform_now'
-end
+# if we need maintenance task run:
+# every 1.day, at: '00:01' do
+#   runner 'TranslationSyncJob.perform_now'
+# end
 
 every 1.day, at: '03:01' do
   runner 'TranslationCacheJob.perform_now'
