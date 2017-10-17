3ce93450d6084aa2d8fb0707e8bc68667ce87311
Jens Struwe <jens@struwe.net>
2017-10-17 18:36:46 +0200

    #358 fix type in controllers skip_before_action

--- app/controllers/api/v1/translation_cache_controller.rb
+++ app/controllers/api/v1/translation_cache_controller.rb
@@ -3,7 +3,7 @@ require 'phrase_app_client' #TODO: could vanish any time, may be
 class Api::V1::TranslationCacheController < Api::V1::BaseController
   include EnsureToken
 
-  skip_before_action :authenticate_api_v1_user!, only: :phraseapp_hook
+  skip_before_action :authenticate_api_v1_user!, only: :phraseapp_webhook
   skip_before_action :ensure_token, except: :phraseapp_webhook
 
   def update
