b76e431e957095e8cdf9243825e0c3af8dbab931
Jens Struwe <jens@struwe.net>
2017-10-17 19:11:13 +0200

    add setting files to spring watcher

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,11 @@
 %w(
   .ruby-version
   .rbenv-vars
+  config/settings.yml
+  config/settings.local.yml
+  config/settings/test.yml
+  config/settings/development.yml
+  config/settings/production.yml
   tmp/restart.txt
   tmp/caching-dev.txt
 ).each { |path| Spring.watch(path) }
