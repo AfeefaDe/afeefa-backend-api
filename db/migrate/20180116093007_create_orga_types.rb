23aef3f743e5edb2c2460b1a3730ffd91acc8b5f
Jens Struwe <jens@struwe.net>
2018-01-17 13:16:59 +0100

    #416 remove orga types offer and department

--- db/migrate/20180116093007_create_orga_types.rb
+++ db/migrate/20180116093007_create_orga_types.rb
@@ -8,10 +8,8 @@ class CreateOrgaTypes < ActiveRecord::Migration[5.0]
     OrgaType.create!(name: 'Root')
     OrgaType.create!(name: 'Organization')
     OrgaType.create!(name: 'Project')
-    OrgaType.create!(name: 'Offer')
     OrgaType.create!(name: 'Location')
     OrgaType.create!(name: 'Network')
-    OrgaType.create!(name: 'Department')
 
     add_reference :orgas, :orga_type, references: :orga_types, index: true, after: :id
 
