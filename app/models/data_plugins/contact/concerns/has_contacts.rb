c3489e3ee89c67d6ab6ebaa699979ae5d9e7c35a
Jens Struwe <jens@struwe.net>
2018-01-31 14:20:02 +0100

    #423 configure polymorphic entry->contact relation

--- app/models/data_plugins/contact/concerns/has_contacts.rb
+++ app/models/data_plugins/contact/concerns/has_contacts.rb
@@ -4,12 +4,13 @@ module DataPlugins::Contact::Concerns::HasContacts
 
   included do
     # ASSOCIATIONS
-    has_many :contacts, class_name: ::DataPlugins::Contact::Contact, foreign_key: :owner_id
+    has_many :contacts, class_name: ::DataPlugins::Contact::Contact, as: :owner
 
     has_one :main_contact, -> { ::DataPlugins::Contact::Contact.main.limit(1) },
-      class_name: ::DataPlugins::Contact::Contact, foreign_key: :owner_id
+      class_name: ::DataPlugins::Contact::Contact, as: :owner
+
     has_many :sub_contacts, -> { ::DataPlugins::Contact::Contact.sub },
-      class_name: ::DataPlugins::Contact::Contact, foreign_key: :owner_id
+      class_name: ::DataPlugins::Contact::Contact, as: :owner
 
     has_many :contact_persons, class_name: ::DataPlugins::Contact::ContactPerson, through: :contacts
   end
