5be04430147a93e6af0740fd98221a07e027c04c
Steve Reinke <steve_is@gmx.de>
2018-02-28 12:13:24 +0100

    [TASK] fix migration stuff

--- db/migrate/20180207125727_create_facets_and_facet_items.rb
+++ db/migrate/20180207125727_create_facets_and_facet_items.rb
@@ -1,26 +1,32 @@
 class CreateFacetsAndFacetItems < ActiveRecord::Migration[5.0]
   def up
-    create_table :facets do |t|
-      t.string :title
+    unless ApplicationRecord.connection.execute('show tables').to_a.inspect =~ /facets/
+      create_table :facets do |t|
+        t.string :title
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
-    create_table :facet_items do |t|
-      t.string :title
-      t.string :color
+    unless ApplicationRecord.connection.execute('show tables').to_a.inspect =~ /facet_items/
+      create_table :facet_items do |t|
+        t.string :title
+        t.string :color
 
-      t.references :facet, index: true
-      t.references :parent, index: true
+        t.references :facet, index: true
+        t.references :parent, index: true
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
-    create_table :owner_facet_items do |t|
-      t.references :owner, polymorphic: true, index: true
-      t.references :facet_item, index: true
+    unless ApplicationRecord.connection.execute('show tables').to_a.inspect =~ /owner_facet_items/
+      create_table :owner_facet_items do |t|
+        t.references :owner, polymorphic: true, index: true
+        t.references :facet_item, index: true
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 
