84c93a884fc5b5abe95be23898c726778a27330a
Steve Reinke <steve_is@gmx.de>
2018-02-28 12:07:00 +0100

    [TASK] fix migration stuff

--- db/migrate/20180214163648_create_chapter_configs.rb
+++ db/migrate/20180214163648_create_chapter_configs.rb
@@ -1,12 +1,18 @@
 class CreateChapterConfigs < ActiveRecord::Migration[5.0]
-  def change
-    create_table :chapter_configs do |t|
-      t.integer :chapter_id
-      t.integer :creator_id
-      t.integer :last_modifier_id
-      t.boolean :active
+  def up
+    unless ApplicationRecord.connection.execute('show tables').to_a.inspect =~ /chapter_configs/
+      create_table :chapter_configs do |t|
+        t.integer :chapter_id
+        t.integer :creator_id
+        t.integer :last_modifier_id
+        t.boolean :active
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
+  end
+
+  def down
+    drop_table :chapter_configs
   end
 end
