c9af8b42fa428aad15986cf433311475edd6f447
Steve Reinke <steve_is@gmx.de>
2018-03-14 16:44:36 +0100

    [FEATURE] add new annotation for external feedback, refs #434

--- db/migrate/20180314142024_add_annotation_category_for_external_feedback.rb
+++ db/migrate/20180314142024_add_annotation_category_for_external_feedback.rb
@@ -1,6 +1,6 @@
 class AddAnnotationCategoryForExternalFeedback < ActiveRecord::Migration[5.0]
   def up
-    AnnotationCategory.create!(title: 'EXTERNE ANMERKUNG')
+    AnnotationCategory.create!(title: 'EXTERNE ANMERKUNG', generated_by_system: true)
   end
 
   def down
