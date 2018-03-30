3992bf2a795c85538718396c043f8ef90fec3f7f
Jens Struwe <jens@struwe.net>
2018-03-31 01:50:13 +0200

    fix migration error (typo)

--- db/migrate/20180326084730_rename_owner_tables.rb
+++ db/migrate/20180326084730_rename_owner_tables.rb
@@ -1,6 +1,6 @@
 class RenameOwnerTables < ActiveRecord::Migration[5.0]
   def change
-    rename_table :offer_owners, :offer_owners
+    rename_table :owner_offers, :offer_owners
     rename_table :facet_item_owners, :facet_item_owners
   end
 end
