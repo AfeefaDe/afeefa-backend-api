8f8dfab2fe3fc08f3fce4e0d2259e76af7927a03
Jens Struwe <jens@struwe.net>
2018-09-15 01:03:18 +0200

    #506 stabilize FAPI json cache

--- app/models/data_modules/offer/offer_owner.rb
+++ app/models/data_modules/offer/offer_owner.rb
@@ -2,5 +2,16 @@ module DataModules::Offer
   class OfferOwner < ApplicationRecord
     belongs_to :offer
     belongs_to :actor, class_name: Orga # TODO: change to DataModules::Actor::Actor
+
+    include FapiCacheable
+
+    def fapi_cacheable_on_save
+      area = Area.find_by(title: actor.area)
+      FapiCacheJob.new.update_all_entries_for_area(area)
+    end
+
+    def fapi_cacheable_on_destroy
+      fapi_cacheable_on_save
+    end
   end
 end
--- app/models/event_host.rb
+++ app/models/event_host.rb
@@ -1,4 +1,16 @@
 class EventHost < ApplicationRecord
   belongs_to :event
   belongs_to :actor, class_name: Orga # TODO: change to DataModules::Actor::Actor
+
+  include FapiCacheable
+
+  def fapi_cacheable_on_save
+    area = Area.find_by(title: actor.area)
+    FapiCacheJob.new.update_all_entries_for_area(area)
+  end
+
+  def fapi_cacheable_on_destroy
+    fapi_cacheable_on_save
+  end
+
 end
