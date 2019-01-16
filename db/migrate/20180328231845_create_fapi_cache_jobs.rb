ee5d60016d8a9943d8252ed9ed225bcf88c5582f
Steve Reinke <steve_is@gmx.de>
2019-01-16 14:40:53 +0100

    andle already done migration which was moved

--- db/migrate/20180328231845_create_fapi_cache_jobs.rb
+++ db/migrate/20180328231845_create_fapi_cache_jobs.rb
@@ -1,16 +1,18 @@
 class CreateFapiCacheJobs < ActiveRecord::Migration[5.0]
   def up
-    create_table :fapi_cache_jobs do |t|
-      t.references :entry, polymorphic: true, index: true
-      t.references :area, index: true
-      t.boolean :updated
-      t.boolean :deleted
-      t.boolean :translated
-      t.string :language
+    unless ActiveRecord::Base.connection.table_exists? 'fapi_cache_jobs'
+      create_table :fapi_cache_jobs do |t|
+        t.references :entry, polymorphic: true, index: true
+        t.references :area, index: true
+        t.boolean :updated
+        t.boolean :deleted
+        t.boolean :translated
+        t.string :language
 
-      t.datetime :created_at
-      t.datetime :started_at
-      t.datetime :finished_at
+        t.datetime :created_at
+        t.datetime :started_at
+        t.datetime :finished_at
+      end
     end
   end
 
