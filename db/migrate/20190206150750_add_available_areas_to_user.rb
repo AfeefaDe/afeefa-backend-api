bf3163ca3e488424c294866e1c7bb47d354f3a08
Steve Reinke <steve_is@gmx.de>
2019-02-25 21:47:49 +0100

    remove migration stuff for first migration of available_areas for struggle on live deployment

--- db/migrate/20190206150750_add_available_areas_to_user.rb
+++ db/migrate/20190206150750_add_available_areas_to_user.rb
@@ -1,10 +1,5 @@
 class AddAvailableAreasToUser < ActiveRecord::Migration[5.0]
   def up
-    add_column :users, :available_areas, :string
-
-    User.find_each do |user|
-      user.initialize_available_areas_by_area!
-    end
   end
 
   def down
