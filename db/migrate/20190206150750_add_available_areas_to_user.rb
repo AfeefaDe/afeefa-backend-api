69169b491c4367fac01a4c5bfe20b9076b99ae2a
Steve Reinke <steve_is@gmx.de>
2019-02-25 21:36:11 +0100

    fix unsupported data type json in old migration for available_areas because of old mysql version on uberspace

--- db/migrate/20190206150750_add_available_areas_to_user.rb
+++ db/migrate/20190206150750_add_available_areas_to_user.rb
@@ -1,7 +1,12 @@
 class AddAvailableAreasToUser < ActiveRecord::Migration[5.0]
   def up
+    add_column :users, :available_areas, :string
+
+    User.find_each do |user|
+      user.initialize_available_areas_by_area!
+    end
   end
 
-  def down 
+  def down
   end
 end
